f931bf663b7cf683f6e88b93ddb8b7b9c25e55d6
Bogdan Kulbida <bkulbida@gmail.com>
2011-04-14 13:19:28 +0300

    fixed installation script.

--- install.rb
+++ install.rb
@@ -1,3 +1,7 @@
 require 'fileutils'
 puts 'Copying configuration file.'
 FileUtils.copy(File.dirname(__FILE__)+'/config/s3fy.yml', "#{RAILS_ROOT}/config/s3fy.yml")
+puts 'Removing default configuration file.'
+FileUtils.remove(File.dirname(__FILE__)+'/config/s3fy.yml')
+FileUtils.remove(File.dirname(__FILE__)+'/config')
+puts 'Plugin has been installed'
