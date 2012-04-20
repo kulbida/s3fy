555007675d0fb92295e47e76aca689754ede7205
Bogdan Kulbida <bkulbida@svitla.com>
2012-04-20 13:04:24 +0300

    Update lib/acts_as_s3fy.rb

--- lib/acts_as_s3fy.rb
+++ lib/acts_as_s3fy.rb
@@ -16,39 +16,38 @@ module S3fy
   module InstanceMethods
     def files_for_store(raw_params)
       # returs formatted params with files only
-      str = raw_params.select {|k,v| k=~/#{AWS_CONFIG[:params_key]}\d*/}.collect{|k,v| v}
+      raw_params.select {|k,v| k=~/#{AWS_CONFIG[:params_key]}\d*/}.collect{|k,v| v}
     end
 
     def has_multiple_files?(raw_params=nil)
       if raw_params
-        return true if files_for_store(raw_params).size > 1
+        return files_for_store(raw_params).size > 1
       else
-        return true if strored_files_to_arr && strored_files_to_arr.size > 1
+        return strored_files_to_arr && strored_files_to_arr.size > 1
       end
       false
     end
 
     def delete_stored_files(files)
       # files - an array of files
-      for file in files do
+      files.each do |file|
         S3 { S3fyCore.delete("#{file}") }
       end
     end
 
     def upload_all_files(files)
-      for file in files do
-        S3 { S3fyCore.store("#{File.basename(file)}", open(file)) }
+      files.each do |firle|
+        S3 { S3fyCore.store(File.basename(file), open(file)) }
       end
     end
 
     def strored_files_to_str(stored_files_arr, base_name_only=false)
       return stored_files_arr.collect{|v| File.basename(v)+'|'}.to_s if base_name_only
-      return stored_files_arr.collect{|v| v+'|'}.to_s
+      stored_files_arr.collect{|v| v+'|'}.to_s
     end
     
     def has_attachments?
-      return true unless read_attribute(self.s3fy_original_path.to_sym).blank?
-      false      
+      read_attribute(self.s3fy_original_path.to_sym).present?
     end
 
     def strored_files_to_arr
@@ -76,7 +75,8 @@ module S3fy
       S3 { S3fyCore.value("#{File.basename(file)}") }
     end
     
-    private
+  private
+
     def S3
       AWS::S3::Base.establish_connection!(
         :access_key_id     => "#{AWS_CONFIG[:access_key_id]}",
